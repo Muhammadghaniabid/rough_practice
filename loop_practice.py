97708086734fb8ef7574e7d8c89a801b0974be89
Muhammad Ghani <muhammadghaniabid@gmail.com>
2026-03-30 05:00:43 -0800

    Update loop_practice.py

--- loop_practice.py
+++ loop_practice.py
@@ -4,3 +4,24 @@ while True:
     todo=input(prompt)
     todos.append(todo)
     print(todos)
+    prompt='Enter you Todo:- '
+todos=[]
+while True:
+    todo=input(prompt)
+    todos.append(todo)
+    print(todos)
+
+    password=input('Enter your password')
+while password !='Ghani':
+    password=input('enter it again')
+print('logged Inn')
+
+x=2
+while x==9:
+     
+     print(x)
+x = 0
+
+while x < 10:
+    print(9*x)
+    x += 1
