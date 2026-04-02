ba2d301589d27ec0ff45d601a4a3de6b07079905
Muhammad Ghani <muhammadghaniabid@gmail.com>
2026-04-02 11:50:00 -0800

    Add files via upload

---
+++ type_conversion.py
@@ -0,0 +1,17 @@
+# x='15'
+# y='25'
+# sum=int(x)+int(y)
+# print(sum)
+
+# str_num='0.15'
+# discount=float(str_num)
+# print(discount)
+
+int_num1=10
+int_num2=20
+int_num3=30
+str_num1=str(int_num1)
+str_num2=str(int_num2)
+str_num3=str(int_num3)
+concatination=str_num1+','+str_num2+','+str_num3
+print(concatination)
