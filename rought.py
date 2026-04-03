7efae8103e42a564f84b8270d7ae0050528afa6d
Muhammad Ghani <muhammadghaniabid@gmail.com>
2026-04-03 13:28:12 -0800

    Add files via upload

---
+++ rought.py
@@ -0,0 +1,28 @@
+# firstname = "John"
+# middlename="Michal"
+# lastname="Smith"
+# person_list=[firstname,middlename,lastname,'Newyork']
+# person=person_list
+# print(person)
+
+# student="John Smith"
+# students=[]
+# students.append(student)
+# print(students)
+
+# person = "sarah"
+# print(person.capitalize())
+
+# button_label = "submit"
+# print(button_label.capitalize())
+
+# sentence = "the sky is blue today."
+# name = "sarah smith"
+
+# # Capitalize first letter of sentence
+# print(sentence.capitalize())
+
+# # Capitalize both words in name
+# print(name.capitalize())
+
+
