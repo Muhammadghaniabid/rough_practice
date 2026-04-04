48a8967550fd14a6eb69ff0614b999e6fb9b0339
Muhammad Ghani <muhammadghaniabid@gmail.com>
2026-04-03 17:04:58 -0800

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,25 @@
+# candies=['candies','toffies','morfies']
+# for candien in candies:
+#     print(candien)
+
+# members = ["john", "sarah", "dora"]
+# for member in members:
+#     print(member.capitalize())
+
+# country = "USA"  # example
+
+# match country:
+#     case "USA" | "United States":
+#         print("Hello")
+#     case "Italy":
+#         print("Ciao")
+#     case "Germany":
+#         print("Hallo")
+
+# employees = ["john smith", "sarah bremen", "dora dawson"]
+# for employee in employees:
+#     print(employee.title())
+# buttons = ["cancel", "reply", "submit"]
+ 
+# for i in buttons:
+#     print(i.capitalize())
